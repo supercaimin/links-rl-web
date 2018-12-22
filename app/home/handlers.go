af7f29793fe9d16271f7a821e19b6fc6f8a09200
caimin <caimin@ibokun.com>
2018-12-22 14:23:57 +0800

    xx

--- app/home/handlers.go
+++ app/home/handlers.go
@@ -17,13 +17,19 @@ type Controller struct {
 // Index home index page
 func (ctrl Controller) Index(w http.ResponseWriter, req *http.Request) {
 	var (
-		Propertys []propertys.Property
-		tx        = eutils.GetDB(req)
+		Propertys1 []propertys.Property
+		Propertys2 []propertys.Property
+		Propertys3 []propertys.Property
+
+		tx = eutils.GetDB(req)
 	)
 
-	tx.Find(&Propertys)
+	tx.Limit(5).Find(&Propertys1, "is_premier_property=? AND is_vaild=?", 1, 1)
+	tx.Offset(5).Limit(5).Find(&Propertys2, "is_premier_property=? AND is_vaild=?", 1, 1)
 
-	ctrl.View.Execute("index", map[string]interface{}{"Propertys": Propertys}, req, w)
+	tx.Order("view_count desc").Limit(6).Find(&Propertys3, "is_premier_property=? AND is_vaild=?", 0, 1)
+
+	ctrl.View.Execute("index", map[string]interface{}{"Propertys1": Propertys1, "Propertys2": Propertys2, "Propertys3": Propertys3}, req, w)
 }
 
 // SwitchLocale switch locale
